dc1370fd82963c364e1180831982d9f00f8eb12f
Omar Jamil <omarj@cs.utexas.edu>
2019-11-25 00:10:49 -0600

    extract cpio archive into ramfs

--- lib/initramfs/initramfs.c
+++ lib/initramfs/initramfs.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <dirent.h>
+
 #include <uk/initramfs.h>
 #include <uk/hexdump.h>
 #include <sys/mount.h>
@@ -10,6 +13,9 @@
 
 #define CPIO_MAGIC_NEWC "070701"
 #define CPIO_MAGIC_CRC "070702"
+#define FILE_TYPE_MASK 0170000
+#define DIRECTORY_BITS 040000
+#define FILE_BITS 0100000
 
 struct cpio_header {
     char magic[6];
@@ -32,7 +38,7 @@ static void hexdump(size_t len, char* loc)
 {
     size_t i;
     for (i = 0; i < len; i++) {
-        printf("%c ", (unsigned char)*(loc+i));
+        printf("%c", (unsigned char)*(loc+i));
     }
     printf("\n");
 }
@@ -63,18 +69,18 @@ static void print_header(struct cpio_header *header)
     hexdump(sizeof(header->ref_major), header->ref_major);
     printf("%s: ", "ref_minor");
     hexdump(sizeof(header->ref_minor), header->ref_minor);
-    printf("%s: ", "namesizeull_terminate");
+    printf("%s: ", "namesize");
     hexdump(sizeof(header->namesize), header->namesize);
     printf("%s: ", "chksum");
     hexdump(sizeof(header->chksum), header->chksum);
 }
 
-static int to_int(size_t len, char *loc)
+static unsigned int to_int(size_t len, char *loc)
 {
     int val = 0;
     size_t i;
     for (i = 0; i < len; i++) {
-        val <<= 1;        
+        val *= 16;        
         if (*(loc+i) >= '0' && *(loc+i) <= '9') {
             val += (*(loc+i) - '0');
         } else {
@@ -92,44 +98,85 @@ static char* null_terminate(size_t len, char *loc)
     return to_return; 
 }
 
-int initramfs_init(struct ukplat_memregion_desc *desc)
+static void* align_4(void* input_ptr)
 {
-    printf("Archive base: %p\n", desc->base);
-    printf("Archive length: %ld\n", desc->len);
+    char* ptr = input_ptr;
+    if ((unsigned long)(ptr)%4 == 0) {
+        return ptr;
+    }
+    else {
+        return ptr + (4-((unsigned long)(ptr)%4));
+    }
+}
+
+static inline char* filename(struct cpio_header *header)
+{
+    return (char*)header + sizeof(struct cpio_header);
+}
+
+static bool is_file(struct cpio_header *header)
+{
+    return (to_int(8, header->mode) & FILE_TYPE_MASK) == FILE_BITS;
+}
+
+static bool is_dir(struct cpio_header *header)
+{
+    return (to_int(8, header->mode) & FILE_TYPE_MASK) == DIRECTORY_BITS;
+}
+
+static mode_t mode(struct cpio_header *header)
+{
+    return (mode_t)to_int(8, header->mode);
+}
+
+static char* absolute_path(char* path)
+{
+    char* abs_path = (char*)malloc(strlen(path)+2);
+    *abs_path = '/';
+    memcpy(abs_path + 1, path, strlen(path));
+    *(abs_path+strlen(path)+1) = '\0';
+    return abs_path;
+}
+
+static struct cpio_header* read_section(struct cpio_header *header)
+{
+    if (strcmp(filename(header), "TRAILER!!!") == 0) {
+        return NULL;
+    }
+    //printf("Header for: %s\n", filename(header));
+    //print_header(header);
+    char* path_from_root = absolute_path(filename(header));
+    if (is_file(header) && to_int(8, header->filesize) != 0) {
+        //printf("Writing to file %s...\n", path_from_root);
+        //TODO: error check the rest of this if
+        int fd = open(path_from_root, O_CREAT | O_RDWR);
+        char *data_location = (char*)align_4((char*)(header) + sizeof(struct cpio_header) + to_int(8, header->namesize));
+        //char *data = null_terminate(to_int(8, header->filesize), data_location);
+        //printf("Data is located at: %p with a length of %d bytes %d \n", data_location, to_int(8, header->filesize), fd);
+        //printf("Data: %s", data);
+        write(fd, data_location, to_int(8, header->filesize));
+        chmod(path_from_root, mode(header) & 0777);
+        close(fd);
+        //free(data);
+    }
+    else if (is_dir(header)) {
+        //printf("Making directory %s...\n", path_from_root);
+        //TODO: error check this
+        mkdir(path_from_root, mode(header) & 0777);
+    }
+    free(path_from_root);
+    struct cpio_header *next_header = (struct cpio_header*)align_4((char*)header + sizeof(struct cpio_header) + to_int(8, header->namesize));   
+    next_header = (struct cpio_header*)align_4((char*)next_header + to_int(8, header->filesize));
+    return next_header;
+}
+
+int initramfs_init(struct ukplat_memregion_desc *desc)
+{   
     struct cpio_header *header = (struct cpio_header *)(desc->base);
-    print_header(header);
-    char* nt_namesize = null_terminate(sizeof(header->namesize), header->namesize);
-    printf("Filename size: %ld\n", strtol(nt_namesize, NULL, 16));
-    free(nt_namesize);
-    int size = to_int(sizeof(header->namesize), header->namesize);
-    printf("Filename size using our method: %d\n", size);
-    printf("Filename: %s\n", (char*)((char*)desc->base + sizeof(struct cpio_header)));
-
-
+    //TODO: error check this
     mount("", "/", "ramfs", 0, NULL);
-    printf("%s\n", mkdir("/testdir", 0x777) < 0 ? "Failed" : "yay");
-    int fd = open("/testdir/testfile", O_CREAT | O_RDWR);
-    if (fd < 0) {
-        printf("bad\n");
+    while (header != NULL && (char*)header <= (char*)desc->base + desc->len) {
+        header = read_section(header);
     }
-    char* msg = "hello, world ramfs!";
-    long len = strlen(msg);
-    char buff[1024];
-    if (write(fd, msg, len) < 0) {
-        printf("write failed\n");
-    }
-    if (lseek(fd, 0, SEEK_SET) < 0) {
-        printf("seek failed\n");
-    }
-    if (read(fd, &buff[0], len) < 0) {
-        printf("read failed\n");
-    }
-    if (close(fd) < 0) {
-        printf("close failed\n");
-    }
-    buff[len + 1] = '\0';
-    printf("%s", &buff[0]);
-    
-
     return 0;
 } 
