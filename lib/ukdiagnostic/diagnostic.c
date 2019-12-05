75ce01d599a03c1bca9051aeb20826c1775969bf
Omar Jamil <omarj@cs.utexas.edu>
2019-12-04 20:43:05 -0600

    uses compiler directives to register functions

--- lib/ukdiagnostic/diagnostic.c
+++ lib/ukdiagnostic/diagnostic.c
@@ -5,12 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <uk/ctors.h>
+
+static void ctor() {
+    printf("Hello?\n");
+}
+
+UK_CTOR_FUNC(1, ctor);
 
 static UK_LIST_HEAD(diagnostic_entries);
 
-int init() {
-    return 0;
-}
 
 /**
  * Local functions
@@ -33,7 +37,18 @@ struct diagnostic_entry* find_entry(char *name) {
 
 int run_diag_function(char *name) {
     struct diagnostic_entry * entry = find_entry(name);
-    int out = entry->func();
+    int out = -1;
+    if (entry != NULL) {
+        int out = entry->func();
+    }
     return out;
 }
+
+static int test_static() {
+    printf("the preprocessor works across classes\n");
+    return 10;
+}
+
+DIAGNOSTIC_FUNCTION("static test", test_static);
+
 
--- lib/ukdiagnostic/include/uk/diagnostic.h
+++ lib/ukdiagnostic/include/uk/diagnostic.h
@@ -1,6 +1,9 @@
 #include <uk/list.h>
 #include <uk/config.h>
 #include <uk/essentials.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <uk/ctors.h>
 
 struct diagnostic_entry {
     char *diag_name;
@@ -10,3 +13,28 @@ struct diagnostic_entry {
 
 void add_entry(struct diagnostic_entry *entry);
 int run_diag_function(char *name);
+
+#define DIAGNOSTIC_FUNCTION(name,fn)  \
+    static struct diagnostic_entry __STRINGCONCAT(DIAG_ENTRY__, fn) = { \
+        .diag_name = name,\
+        .func = &fn,   \
+        .next = NULL    \
+    }; \
+    void __CONSTRUCTOR_NAME(fn)() { \
+        printf("REGISTERED FUNCTION %s\n", name);   \
+        add_entry(&__STRINGCONCAT(DIAG_ENTRY__, fn)); \
+    } \
+    UK_CTOR_FUNC(1, __CONSTRUCTOR_NAME(fn));
+
+#define __STRINGCONCAT(x, y) x ## y
+
+#define __CONSTRUCTOR_NAME(name) \
+    __STRINGCONCAT(__CONSTRUCTOR__, name)
+
+
+int headerfunc(){
+    printf("yo!\n");
+    return 1;
+}
+
+DIAGNOSTIC_FUNCTION("Diagnostic",headerfunc);
