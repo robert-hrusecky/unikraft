73d5eccf09ac2a70fdc95be1b316b52d400da938
Robert Hrusecky <robert.hrusecky@utexas.edu>
2019-12-08 22:50:54 -0600

    update parser with better error handleing

--- lib/ukdiagnostic/json_frontend.c
+++ lib/ukdiagnostic/json_frontend.c
@@ -5,52 +5,67 @@
 #include <string.h>
 #include <stdbool.h>
 
+struct json_parser_state {
+    const char* data;
+    size_t len;
+    size_t pos;
+    bool error;
+};
+
 static void free_json_object(struct json_object* object);
 static void free_json_array(struct json_array* array);
-static struct json_value* parse_value(const char* data, const size_t len, size_t* pos);
-static char* parse_string(const char* data, const size_t len, size_t* pos);
+static struct json_value* parse_value(struct json_parser_state* state);
+static char* parse_string(struct json_parser_state* state);
 
-static bool check_end(size_t len, size_t pos) {
-    bool good = pos < len;
-    if (!good) {
-        printf("Unexpectedly reached end of json data\n");
-    }
-    return good;
+static bool check_end(struct json_parser_state* state) {
+    state->error = state->pos >= state->len;
+    return !state->error;
 }
 
-static bool check_char(const char* data, size_t len, size_t pos, char check) {
-    if (!check_end(len, pos))
+static bool expect_char(struct json_parser_state* state, char check) {
+    if (!check_end(state))
         return false;
-    bool good = data[pos] == check;
-    if (!good) {
-        printf("Unexpected character '%c' in json; expected '%c'\n", data[pos], check);
-    }
-    return good;
+    state->error = state->data[state->pos] != check;
+    if (!state->error)
+        state->pos++;
+    return !state->error;
 }
 
-static void parse_ws(const char* data, const size_t len, size_t* pos) {
-    while (*pos < len && isspace(data[*pos])) {
-        (*pos)++;
+static void parse_ws(struct json_parser_state* state) {
+    while (state->pos < state->len && isspace(state->data[state->pos])) {
+        state->pos++;
     }
 }
 
-static struct json_value* parse_element(const char* data, const size_t len, size_t* pos) {
-    parse_ws(data, len, pos);
-    struct json_value* value = parse_value(data, len, pos);
-    parse_ws(data, len, pos);
+static struct json_value* parse_element(struct json_parser_state* state) {
+    parse_ws(state);
+    struct json_value* value = parse_value(state);
+    if (state->error) {
+        free_json_value(value);
+        return NULL;
+    }
+    parse_ws(state);
     return value;
 }
 
-static struct json_object* parse_member(const char* data, const size_t len, size_t* pos) {
-    parse_ws(data, len, pos);
-    char* key = parse_string(data, len, pos);
-    parse_ws(data, len, pos);
-    if (!check_char(data, len, *pos, ':')) {
+static struct json_object* parse_member(struct json_parser_state* state) {
+    parse_ws(state);
+    char* key = parse_string(state);
+    if (state->error) {
         free(key);
         return NULL;
     }
-    (*pos)++; // :
-    struct json_value* value = parse_element(data, len, pos);
+    parse_ws(state);
+    if (!expect_char(state, ':')) {
+        free(key);
+        return NULL;
+    }
+    struct json_value* value = parse_element(state);
+    if (state->error) {
+        free(key);
+        free_json_value(value);
+        return NULL;
+    }
     struct json_object* object = malloc(sizeof(struct json_object));
     object->key = key;
     object->value = value;
@@ -58,86 +73,112 @@ static struct json_object* parse_member(const char* data, const size_t len, size
     return object;
 }
 
-static struct json_object* parse_object(const char* data, const size_t len, size_t* pos) {
-    if (!check_char(data, len, *pos, '{')) {
+static struct json_object* parse_object(struct json_parser_state* state) {
+    if (!expect_char(state, '{')) {
         return NULL;
     }
-    (*pos)++; // {
-    parse_ws(data, len, pos);
-    if (*pos >= len || data[*pos] == '}') {
-        // This is an empty object (unless EOF)
-        check_end(len, *pos);
-        (*pos)++; // }
+    parse_ws(state);
+    if (!check_end(state))
+        return NULL;
+    if (state->data[state->pos] == '}') {
+        // This is an empty object
+        state->pos++; // }
         return NULL;
     }
     // Otherwise, we have a head member
-    struct json_object* head = parse_member(data, len, pos);
-    struct json_object* curr = head;
-    while(data[*pos] != '}') {
-        if (!check_char(data, len, *pos, ',')) {
-            free_json_object(head);
-            return NULL;
-        }
-        (*pos)++; // ,
-        // get the next member, and append to the list
-        curr->next = parse_member(data, len, pos);
-        curr = curr->next;
-    }
-    if (!check_char(data, len, *pos, '}')) {
+    struct json_object* head = parse_member(state);
+    if (state->error) {
         free_json_object(head);
         return NULL;
     }
-    (*pos)++; // }
+    struct json_object* curr = head;
+    while(state->data[state->pos] != '}') {
+        if (!expect_char(state, ',')) {
+            free_json_object(head);
+            return NULL;
+        }
+        // get the next member, and append to the list
+        curr->next = parse_member(state);
+        if (state->error) {
+            free_json_object(head);
+            return NULL;
+        }
+        curr = curr->next;
+    }
+    if (!expect_char(state, '}')) {
+        free_json_object(head);
+        return NULL;
+    }
     return head;
 }
 
-static struct json_array* parse_array(const char* data, const size_t len, size_t* pos) {
+static struct json_array* parse_array(struct json_parser_state* state) {
+    // TODO: support arrays
+    state->error = true;
     return NULL;
 }
 
-static char* parse_string(const char* data, const size_t len, size_t* pos) {
-    if (!check_char(data, len, *pos, '"'))
+static char* parse_string(struct json_parser_state* state) {
+    if (!expect_char(state, '"'))
         return NULL;
-    (*pos)++; // "
 
     // first count the chars
+    size_t start_pos = state->pos;
     size_t count;
-    size_t i;
-    for (i = *pos, count = 0; i < len && data[i] != '"'; i++, count++) {
-        if (data[i] == '\\') {
-            i++;
+    for (count = 0;
+         state->pos < state->len && state->data[state->pos] != '"';
+         state->pos++, count++) {
+        if (state->data[state->pos] == '\\') {
+            // skip escaped chars
+            state->pos++;
         }
     }
 
-    if (!check_end(len, i)) {
+    if (!check_end(state)) {
         return NULL;        
     }
 
+    // rewind to the start of the string
+    state->pos = start_pos;
+
     char* string = malloc(sizeof(char) * (count + 1));
-    for (size_t str_pos = 0; data[*pos] != '"'; (*pos)++, str_pos++) {
-        if (data[*pos] == '\\') {
-            (*pos)++;
+    for (size_t str_pos = 0;
+         state->data[state->pos] != '"';
+         state->pos++, str_pos++) {
+        if (state->data[state->pos] == '\\') {
+            // TODO: Add proper escape character handling
+            // Just skip backslashes for now
+            state->pos++;
         }
-        string[str_pos] = data[*pos];
+        string[str_pos] = state->data[state->pos];
     }
     string[count] = '\0';
-    if (!check_char(data, len, *pos, '"'))
+    if (!expect_char(state, '"')) {
+        free(string);
         return NULL;
-    (*pos)++; // "
+    }
     return string;
 }
 
-static int64_t parse_int(const char* data, const size_t len, size_t* pos) {
-    if (!check_end(len, *pos))
+static int64_t parse_int(struct json_parser_state* state) {
+    // TODO: Use stdlib to parse this?
+    if (!check_end(state))
         return 0;
-    int negative = data[*pos] == '-';
+    bool negative = state->data[state->pos] == '-';
     if (negative)
-        (*pos)++; // -
+        state->pos++; // -
+
+    if (!check_end(state))
+        return 0;
+    if (!isdigit(state->data[state->pos])) {
+        state->error = true;
+        return 0;
+    }
 
     int64_t num = 0;
-    for (; *pos < len && isdigit(data[*pos]); (*pos)++) {
+    for (; state->pos < state->len && isdigit(state->data[state->pos]); state->pos++) {
         num *= 10;
-        num += data[*pos] - '0';
+        num += state->data[state->pos] - '0';
     }
     if (negative)
         num *= -1;
@@ -150,11 +191,11 @@ static struct json_value* create_json_value() {
     return value;
 }
 
-static struct json_value* parse_value(const char* data, const size_t len, size_t* pos) {
-    if (!check_end(len, *pos))
+static struct json_value* parse_value(struct json_parser_state* state) {
+    if (!check_end(state))
         return NULL;
-    struct json_value* value = create_json_value(JSON_NULL);
-    char next_char = data[*pos];
+    struct json_value* value = create_json_value();
+    char next_char = state->data[state->pos];
     // object: '{'
     // array: '['
     // string: '"'
@@ -165,39 +206,55 @@ static struct json_value* parse_value(const char* data, const size_t len, size_t
     switch (next_char) {
         case '{':
             value->type = JSON_OBJECT;
-            value->object = parse_object(data, len, pos);
+            value->object = parse_object(state);
             break;
         case '[':
             value->type = JSON_ARRAY;
-            value->array = parse_array(data, len, pos);
+            value->array = parse_array(state);
             break;
         case '"':
             value->type = JSON_STRING;
-            value->string = parse_string(data, len, pos);
+            value->string = parse_string(state);
             break;
         case 't':
             value->type = JSON_TRUE;
-            *pos += 4;
+            state->pos += 4;
             break;
         case 'f':
             value->type = JSON_FALSE;
-            *pos += 5;
+            state->pos += 5;
             break;
         case 'n':
             value->type = JSON_NULL;
-            *pos += 4;
+            state->pos += 4;
             break;
         default:
             value->type = JSON_INT;
-            value->integer = parse_int(data, len, pos);
+            value->integer = parse_int(state);
+    }
+    
+    if (state->error) {
+        free_json_value(value);
+        return NULL;
     }
     return value;
 }
 
 
 struct json_value* parse_json(const char* data, const size_t len) {
-    size_t pos = 0;
-    return parse_value(data, len, &pos);
+    struct json_parser_state state = {
+        .data = data,
+        .len = len,
+        .pos = 0,
+        .error = false,
+    };
+    struct json_value* result = parse_value(&state);
+
+    if (!state.error)
+        return result;
+
+    free_json_value(result);
+    return create_json_value(JSON_ERROR);
 }
 
 static void free_json_object(struct json_object* object) {
@@ -216,6 +273,7 @@ static void free_json_array(struct json_array* array) {
 }
 
 void free_json_value(struct json_value* value) {
+    if (value == NULL) return;
     switch (value->type) {
         case JSON_OBJECT:
             free_json_object(value->object);
